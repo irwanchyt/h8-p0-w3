a4464f8c45d9ed764b84d38068734c966eb5a8d3
irwancahyt <sdirwancahyanto@gmail.com>
2018-07-09 20:44:58 +0700

    tugas hari selasa

---
+++ exercises-1.js
@@ -0,0 +1,15 @@
+function balikString(kata){
+
+ var kataBaru = ''
+ var sum = kata.length-1
+
+for ( var i = sum ; i >=0 ; i--){
+ kataBaru += kata[i]
+ //console.log(kataBaru)
+
+}
+//console.log(kataBaru)
+return(kataBaru)
+}
+
+balikString('Hello World!')
